14b810091de3480539dc259ce076716c789ca0d6
Roy Storey <kiwiroy@users.noreply.github.com>
2016-12-02 08:01:00 +1300

    check return status and include stats

--- lib/Resolver.pm
+++ lib/Resolver.pm
@@ -15,20 +15,35 @@ has user_agent => sub {
   return Mojo::UserAgent->new();
 };
 
+sub _call {
+  my ($self, $url) = @_;
+  my $resp = $self->user_agent->get($url => {Accept => '*/*'})->res;
+  return ($resp->code == 200, $resp->body);
+}
 sub resolve {
   my ($self, $compound) = @_;
   my $url = $self->url_generator->($compound, 'cas');
-  my $cas_list = $self->user_agent->get($url => {Accept => '*/*'})->res->body;
-  $compound->cas_list([ split /\n/, $cas_list ]);
+  my ($ok, $cas_list) = $self->_call($url);
+  if ($ok) {
+    $compound->cas_list([ split /\n/, $cas_list ]);
 
-  $url = $self->url_generator->($compound, 'iupac_name');
-  my $iupac = $self->user_agent->get($url => {Accept => '*/*'})->res->body;
-  $compound->iupac_name( $iupac );
+    $url = $self->url_generator->($compound, 'iupac_name');
+    my ($ok1, $iupac) = $self->_call($url);
+    $compound->iupac_name( $iupac );
 
-  $url = $self->url_generator->($compound, 'names');
-  my $synonyms = $self->user_agent->get($url => {Accept => '*/*'})->res->body;
-  $compound->synonyms([ split /\n/, $synonyms ]);
+    $url = $self->url_generator->($compound, 'names');
+    my ($ok2, $synonyms) = $self->_call($url);
+    $compound->synonyms([ split /\n/, $synonyms ]);
+    $self->{'found'}++;
+  } else {
+    $self->{'missing'}++;
+  }
   return $self;
+}
+
+sub stats {
+  my $self = shift;
+  return { map { $_ => $self->{$_} } qw{found missing} };
 }
 
 1;
